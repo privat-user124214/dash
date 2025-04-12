5aebb6176b67610785cf16b0775c65aedfbda25f
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-13 00:48:10 +0200

    Update config.php

--- public_html/dash/config.php
+++ public_html/dash/config.php
@@ -1,5 +1,5 @@
 <?php
-// Discord API-Konfigurationsdaten
+// Discord API-Konfigurationsdaten 
 define('CLIENT_ID', '1284484623279067196'); // Ersetze mit deiner Discord-Anwendungs-Client-ID
 define('CLIENT_SECRET', 'rWr9kGV33-rTIvhv6ACi-NrJVxbQFQKy'); // Ersetze mit deinem Discord-Client-Secret
 define('REDIRECT_URI', 'https://dash.novarix-studio.de/callback.php'); // Deine Redirect-URI
