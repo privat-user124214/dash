268d7979bac748fad30485e4e285465a12209058
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-13 00:39:33 +0200

    Update dashbord.php

--- dashbord.php
+++ dashbord.php
@@ -2,7 +2,7 @@
 // Beispiel-Daten, die aus der Callback-Datei gespeichert wurden
 $userData = json_decode(file_get_contents('user_data.json'), true);
 
-// Benutzerinformation anzeigen
+// Benutzerinformation anzeigen 
 echo '<h1>Dashboard</h1>';
 echo '<h2>Benutzername: ' . htmlspecialchars($userData['username']) . '</h2>';
 echo '<p>Email: ' . htmlspecialchars($userData['email']) . '</p>';
