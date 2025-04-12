cbd4577af448e86afdac8ba27771fedd06a63eb7
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-13 01:22:47 +0200

    Update config.php

--- public_html/dash/config.php
+++ public_html/dash/config.php
@@ -2,6 +2,6 @@
 // Discord API-Konfigurationsdaten 
 define('CLIENT_ID', '1284484623279067196'); // Ersetze mit deiner Discord-Anwendungs-Client-ID
 define('CLIENT_SECRET', 'rWr9kGV33-rTIvhv6ACi-NrJVxbQFQKy'); // Ersetze mit deinem Discord-Client-Secret
-define('REDIRECT_URI', 'https://dash.novarix-studio.de/callback.php'); // Deine Redirect-URI
+define('REDIRECT_URI', 'http://novarix-studio.great-site.net/callback.php'); // Deine Redirect-URI
 define('DISCORD_API_URL', 'https://discord.com/api/v10');
 ?>
