5e8fdd3ead5b496386cd0667c05f647de9a831f2
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-13 05:08:50 +0200

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$client_id = '1284484623279067196';
-$client_secret = 'rWr9kGV33-rTIvhv6ACi-NrJVxbQFQKy';
-$redirect_uri = 'https://dash-k7s3.onrender.com/callback.php'; // Auf Render: http://dein-service.onrender.com/callback.php
-?>
