e6642251a86af629f11c0740b4ca2f44a7de1ff2
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-13 18:16:03 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,12 @@
 <?php
-$host = "sql303.infinityfree.com"; // Ersetze mit deinem Host
+$host = "sql303.infinityfree.com";
 $dbname = "if0_38729105_logs";
 $username = "if0_38729105";
 $password = "p3Fuvu1gawZo";
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Verbindung fehlgeschlagen: " . $e->getMessage());
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
 ?>
