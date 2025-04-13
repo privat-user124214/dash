2160e21d8bab4976d926e620d9002a3add778cb3
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-13 05:56:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,8 @@ $discord_login_url = "https://discord.com/oauth2/authorize?client_id=$client_id&
     </header>
     <main style="text-align: center; padding-top: 100px;">
         <h2>Willkommen beim Novarix Dashboard</h2>
-        <a href="https://discord.com/oauth2/authorize?client_id=1284484623279067196&permissions=0&response_type=code&redirect_uri=https%3A%2F%2Fdash-k7s3.onrender.com%2Fcallback.php&integration_type=0&scope=identify+email+connections+bot+guilds+applications.commands+guilds.members.read">
+        <a href="https://discord.com/oauth2/authorize?client_id=1284484623279067196&permissions=0&response_type=code&redirect_uri=https%3A%2F%2Fdash-k7s3.onrender.com%2Fcallback.php&integration_type=0&scope=identify+email+connections+guilds+guilds.members.read
+">
             <img src="https://discord.com/assets/bb408e0343ddedc0967f246f7e89cebf.svg" width="200" alt="Login mit Discord">
         </a>
         <p>Klicke auf das Discord-Logo, um dich anzumelden</p>
