0490638f54658245cd1a9f8e0216e2c4eba4d2a9
user-z55554dfgfh <novarix.studio@gmail.com>
2025-04-13 03:00:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,4 @@ $discord_url = "https://discord.com/oauth2/authorize?" . http_build_query([
     'scope'         => 'identify email connections guilds guilds.members.read'
 ]);
 ?>
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <title>Login mit Discord</title>
-</head>
-<body>
-    <h1>Login Dashboard</h1>
-    <a href="<?= $discord_url ?>">
-        <img src="https://discord.com/assets/bb408e0343ddedc0967f246f7e89cebf.svg" width="200">
-    </a>
-</body>
-</html>
+<a href="<?= $discord_url ?>">Mit Discord einloggen</a>
